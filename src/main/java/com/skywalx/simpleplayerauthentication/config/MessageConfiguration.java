effef685a72f201e8097e0c2801a4bff36da4f63
HungryDev <robinpeeters1401@gmail.com>
2022-12-06 20:52:34 +0100

    Cleanup unused imports

--- src/main/java/com/skywalx/simpleplayerauthentication/config/MessageConfiguration.java
+++ src/main/java/com/skywalx/simpleplayerauthentication/config/MessageConfiguration.java
@@ -1,7 +1,6 @@
 package com.skywalx.simpleplayerauthentication.config;
 
 import me.clip.placeholderapi.PlaceholderAPI;
-import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
